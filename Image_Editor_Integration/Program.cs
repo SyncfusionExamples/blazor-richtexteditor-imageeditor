84387045337c36ca1b1f79d3800875e735697f6d
Bhuvaneshwari-SF4208 <bhuvaneshwari.t@syncfusion.com>
2026-02-12 22:21:09 +0530

    1008006: Add Integration Documentation for Embedly and Image Editor in Syncfusion Rich Text Editor Blazor

--- Image_Editor_Integration/Program.cs
+++ Image_Editor_Integration/Program.cs
@@ -7,7 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+// Register Syncfusion Blazor services
+// Note: For production use, register your Syncfusion license key:
+// Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("YOUR_LICENSE_KEY");
 builder.Services.AddSyncfusionBlazor();
+// Configure SignalR HubOptions to allow messages up to 1 MB in size.
+// This sets the maximum allowed size for incoming messages (default is ~32 KB).
 builder.Services.Configure<HubOptions>(o => o.MaximumReceiveMessageSize = 1024 * 1024);
 
 var app = builder.Build();
